fcc417744411d31fdb2c7b4ac4d30dd82765c285
Lazaro Pereira <spmlaz@gmail.com>
2016-10-30 11:42:53 +0000

    Bookstore Project passing all tests using dictionaries

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,53 @@
 def create_bookstore(name):
-    pass
+    bookstore = {'name' : name, 'last_author_id': 0,'last_book_id': 0,'authors': {},'books':{}}
+    return bookstore
+
 
 def add_author(bookstore, name, nationality):
-    pass
+    authorid = bookstore['last_author_id']
+    bookstore['authors'].update({bookstore['last_author_id']: {'name': name, 'nationality': nationality,'id':authorid}})
+    bookstore['last_author_id'] += 1
+    return bookstore['authors'][authorid]
 
 
 def get_author_by_name(bookstore, name):
-    pass
-
+    for items in bookstore['authors']:
+        for names in bookstore['authors'][items]:
+            if bookstore['authors'][items][names] == name:
+                return bookstore['authors'][items]
+    
 
 def get_author_by_id(bookstore, author_id):
-    pass
+    for items in bookstore['authors']:
+        if items == author_id:
+            return bookstore['authors'][items]
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
-
+    bookid = bookstore['last_book_id']
+    bookstore['books'].update({bookstore['last_book_id']: {'title': title, 'isbn': isbn,'author_id':author_id,'id': bookid}})
+    bookstore['last_book_id'] += 1
+    return bookstore['books'][bookid]
+    
 
 def get_book_by_title(bookstore, title):
-    pass
+    for items in bookstore['books']:
+        for names in bookstore['books'][items]:
+            if bookstore['books'][items][names] == title:
+                return bookstore['books'][items]
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
+    for items in bookstore['books']:
+       if items == book_id:
+           return bookstore['books'][items]
 
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    list1 = []
+    for items in bookstore['books']:
+        if bookstore['books'][items]['author_id'] == author_id:
+            list1.append(bookstore['books'][items])
+    return list1
+    
+    
