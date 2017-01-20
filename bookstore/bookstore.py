1c21180b8e5584b691f81ac88b4b40b4bbb38443
Michelangelo Prisciano <mike.prisciano@icloud.com>
2017-01-20 01:47:45 +0000

    First commit

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,8 +1,26 @@
-def create_bookstore(name):
-    pass
+import time
+
+def create_bookstore():
+    name = {}
+    return name
 
 def add_author(bookstore, name, nationality):
-    pass
+
+    my_time = time.time()
+    
+    my_dic = {}
+    new_author = {
+            'name' : name, 
+            'nationality' : nationality,
+            'id' : my_time, 
+            'books' : my_dic
+        }
+
+    bookstore[name] = new_author
+
+    
+
+    return bookstore[name]
 
 
 def get_author_by_name(bookstore, name):
@@ -14,7 +32,21 @@ def get_author_by_id(bookstore, author_id):
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    
+    book_id = time.time()
+
+    for i in bookstore : 
+        if bookstore[i]['id'] == author_id : 
+            book = {
+
+                #'title' : title,
+                'isbn' : isbn,
+                'id' : book_id
+
+            }
+
+            bookstore[i]['books'][title] = book 
+    return bookstore[i]['books'][title]
 
 
 def get_book_by_title(bookstore, title):
