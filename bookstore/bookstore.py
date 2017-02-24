65abbe2902f8f3de03dc310baf5191334bb7c7f1
Viet Pham <viethuupham@gmail.com>
2017-02-24 01:40:57 +0000

    initial commit

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,45 @@
 def create_bookstore(name):
-    pass
-
+    return {'name': name, 'authors': [], 'books':[]}
+    
 def add_author(bookstore, name, nationality):
-    pass
+    author = {'name': name, 'nationality': nationality, 'id':len(bookstore['authors'])}
+    bookstore['authors'].append(author)
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
 
 
 def get_author_by_id(bookstore, author_id):
-    pass
+    for author in bookstore['authors']:
+        if author['id'] == author_id:
+            return author
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    book = {'title': title, 'isbn': isbn, 'author_id': author_id, 'id':len(bookstore['books'])}
+    bookstore['books'].append(book)
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
+    for book in bookstore['books']:
+        if book['id'] == book_id:
+            return book
 
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    list_book = []
+    for book in bookstore['books']:
+        if book['author_id'] == author_id:
+            list_book.append(book)
+    return list_book
