256ff8f802835c921082df94003672cb0863b45c
Joey <joey.coakley@gmail.com>
2017-05-04 02:53:03 +0000

    Joey Bookstore

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,83 @@
+author_id_generator = 1000
+book_id_generator = 2000
+
 def create_bookstore(name):
-    pass
+    if isinstance(name, str):
+    	bookstore = {
+    		"name" : name,
+    		"authors": {},
+    		"books": {}
+    	}
+    	return bookstore
+    else:
+    	"Invalid entry. Accepted input will be a string"
 
 def add_author(bookstore, name, nationality):
-    pass
+    global author_id_generator
+    bookstore["authors"][name] = {
+    	"name" : name,
+    	"nationality": nationality,
+    	"id": author_id_generator
+    	}
+    author_id_generator += 1
+    return bookstore["authors"][name]
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    if name in bookstore["authors"]:
+    	return bookstore["authors"][name]
+    else:
+    	return "author not found"
+
 
 
 def get_author_by_id(bookstore, author_id):
-    pass
+    for key, value in bookstore['authors'].items():
+		if str(author_id) in str(value['id'])\
+		and len(range(author_id)) == len(range(value['id'])):
+			return bookstore['authors'][key]
+		else:
+			pass
+		return "author ID not found."
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    global book_id_generator
+    bookstore["books"][title] = {
+    "title": title,
+    "isbn": isbn,
+    "author_id": author_id,
+    "id": book_id_generator
+    }
+    book_id_generator += 1
+    return bookstore['books'][title]
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    if title in bookstore['books']:
+    	return bookstore['books'][title]
+    else:
+    	return title + " not found!"
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
-
+    for key, value in bookstore['books'].items():
+    	if str(book_id) in str(value['id'])\
+    	and len(range(book_id)) == len(range(value['id'])):
+    		return bookstore['books'][key]
+    	else:
+    		pass
+    return "book ID not found."
 
 def get_books_by_author(bookstore, author_id):
-    pass
+  	books_by_author = []
+	for key, value in bookstore['books'].items():
+		if str(author_id) in str(value['author_id'])\
+		and len(str(author_id)) == len(str(value['author_id'])):
+			books_by_author.append(value)
+		else:
+			pass
+	if len(books_by_author) > 0:
+	    return books_by_author
+	else:
+	    return "No books by that author found!"
