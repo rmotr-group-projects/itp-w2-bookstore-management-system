ce3f4589fa07d7b5f47dd3267d34ee6e94b0711d
Joey <joey.coakley@gmail.com>
2017-05-04 14:10:46 +0000

    Cleaned up logic. Made this way harder than it needed to be.

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -34,13 +34,11 @@ def get_author_by_name(bookstore, name):
 
 def get_author_by_id(bookstore, author_id):
     for key, value in bookstore['authors'].items():
-        #Check the value, but also make sure that they're the same length.
-        #Otherwise, if author_id was 2001, we'd get a hit on 2, 20, and 200.
-        if str(author_id) in str(value['id']) and len(range(author_id)) == len(range(value['id'])):
-            return bookstore['authors'][key]
+        if value['id'] == author_id:
+            return value
         else:
             pass
-        return "author ID not found."
+        return "Author ID not found"
 
 
 def add_book(bookstore, title, isbn, author_id):
@@ -64,8 +62,8 @@ def get_book_by_title(bookstore, title):
 
 def get_book_by_id(bookstore, book_id):
     for key, value in bookstore['books'].items():
-    	if str(book_id) in str(value['id']):
-    		return bookstore['books'][key]
+    	if book_id == value['id']:
+    		return value
     	else:
     		pass
     return "book ID not found."
@@ -75,7 +73,7 @@ def get_books_by_author(bookstore, author_id):
     for key, value in bookstore['books'].items():
         #Check the value, but also make sure that they're the same length.
         #Otherwise, if author_id was 2001, we'd get a hit on 2, 20, and 200.
-        if str(author_id) in str(value['author_id']) and len(str(author_id)) == len(str(value['author_id'])):
+        if author_id == value['author_id']:
             books_by_author.append(value)
         else:
             pass
