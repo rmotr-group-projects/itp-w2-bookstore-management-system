81aeb4173d54ef5a333f498bf83c1a6038dc21fb
Joey <joey.coakley@gmail.com>
2017-05-04 04:01:57 +0000

    After sorting for Python3

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -33,6 +33,8 @@ def get_author_by_name(bookstore, name):
 
 def get_author_by_id(bookstore, author_id):
     for key, value in bookstore['authors'].items():
+        #Check the value, but also make sure that they're the same length.
+        #Otherwise, if author_id was 2001, we'd get a hit on 2, 20, and 200.
         if str(author_id) in str(value['id']) and len(range(author_id)) == len(range(value['id'])):
             return bookstore['authors'][key]
         else:
@@ -70,11 +72,13 @@ def get_book_by_id(bookstore, book_id):
 def get_books_by_author(bookstore, author_id):
     books_by_author = []
     for key, value in bookstore['books'].items():
+        #Check the value, but also make sure that they're the same length.
+        #Otherwise, if author_id was 2001, we'd get a hit on 2, 20, and 200.
         if str(author_id) in str(value['author_id']) and len(str(author_id)) == len(str(value['author_id'])):
             books_by_author.append(value)
         else:
             pass
     if len(books_by_author) > 0:
-        return books_by_author
+        return sorted(books_by_author, key=lambda k: k['title'])
     else:
         return "No books by that author found!"
