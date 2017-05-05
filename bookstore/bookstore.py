a165676ee0d16fb9468ec1cce33e1fb5723667ac
Danny C <brasqo@gmail.com>
2017-05-05 02:54:05 +0000

    Ike, Danny, Charlie bookstore

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,69 @@
 def create_bookstore(name):
-    pass
-
+    bookstore = {'name' : name,
+                'authors' : [],
+                'books' : [],
+                'author_id': 0,
+                'book_id' : 0
+    }
+    return bookstore
+    
 def add_author(bookstore, name, nationality):
-    pass
-
+    author = {'name' : name,
+              'nationality': nationality,
+              'id': bookstore['author_id'],
+    }
+    
+    bookstore['author_id'] += 1
+    bookstore['authors'].append(author) 
+    
+    return author
+                    
 
 def get_author_by_name(bookstore, name):
-    pass
 
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
+            
 
 def get_author_by_id(bookstore, author_id):
-    pass
+    
+    for author in bookstore['authors']:
+        if author['id'] == author_id:
+            return author
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    book = {'title' : title,
+            'isbn' : isbn,
+            'id' : bookstore['book_id'],
+            'author_id': author_id
+    }
+    
+    bookstore['book_id'] += 1
+    bookstore['books'].append(book) 
+    
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
+    
+    for book in bookstore['books']:
+        if book['id'] == book_id:
+            return book
 
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    
+    bookslist = []
+    for book in bookstore['books']:
+        if book['author_id'] == author_id:
+            bookslist.append(book)
+    return bookslist
