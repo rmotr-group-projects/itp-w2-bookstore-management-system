227c662a121aab7edf4c56cdc7f2d11fd8d40171
James Curbow <curbowj@uni.edu>
2017-06-09 02:22:00 +0000

    bookstore management systems Ana, Sayid, and James

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,69 @@
+
+
 def create_bookstore(name):
-    pass
+   
+    bookstore = {'name': name, 
+                 'authors': [], 
+                 'books' : [],
+                 'author_id' : 0,
+                 'book_id' : 0
+                }
+                
+    return bookstore
 
 def add_author(bookstore, name, nationality):
-    pass
-
-
+    
+    author = {'name': name, 
+              'nationality' : nationality, 
+              'id' : bookstore['author_id'] 
+             }
+             
+    bookstore['author_id'] += 1
+    bookstore['authors'].append(author)
+    return author
+    
 def get_author_by_name(bookstore, name):
-    pass
-
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
+    
 
 def get_author_by_id(bookstore, author_id):
-    pass
-
+    for author in bookstore['authors']: 
+        if author['id'] == author_id:
+            return author
+        
+    #when pass autho_id
+    #check bookstore if there
+    #if id = id in the bookstore
+    #return AUTHOR!!!! NOT ID - I need to read directions more...
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
-
+    books = {
+            'title': title,
+            'isbn': isbn,
+            'author_id': author_id,
+            'id' : bookstore['book_id']
+            }
+            
+    bookstore['book_id'] += 1        
+    bookstore['books'].append(books)
+    return books
 
 def get_book_by_title(bookstore, title):
-    pass
+    for books in bookstore['books']:
+        if books['title'] == title:
+            return books
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
-
+    for books in bookstore['books']: 
+        if books['id'] == book_id:
+            return books
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    requested_books = []
+    for book in bookstore['books']: 
+        if book['author_id'] == author_id:
+            requested_books.append(book)
+    return requested_books        
