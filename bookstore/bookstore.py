ac038ac92f2009b06cc75369935a4a13974474f8
Kyle <mcdougle.kyle@gmail.com>
2017-06-09 01:57:37 +0000

    this is the final project

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,63 @@
 def create_bookstore(name):
-    pass
+    store = {'name' : name,
+            'authors' : {},
+            'books' : {},
+            'author_id' : 0,
+            'book_id' : 0
+    }
+    return store
 
 def add_author(bookstore, name, nationality):
-    pass
-
-
+    author = {}
+    author["name"] = name
+    author["nationality"] = nationality 
+    author['id'] = bookstore['author_id']
+    bookstore['author_id'] += 1
+    bookstore['authors'][name] = author
+    return author
+    
 def get_author_by_name(bookstore, name):
-    pass
+    for author_name, author in bookstore['authors'].items():
+      if author['name'] is name:
+       return author
 
 
 def get_author_by_id(bookstore, author_id):
-    pass
+    for author_name, author in bookstore['authors'].items():
+      if author['id'] == author_id:
+       return author
 
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    book = {}
+    book["title"] = title
+    book["isbn"] = isbn 
+    book['id'] = bookstore['book_id']
+    book['author_id'] = author_id
+    bookstore['book_id'] += 1
+    bookstore['books'][title] = book
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    for book_name, book in bookstore['books'].items():
+        if book['title'] is title:
+            return book
 
 
 def get_book_by_id(bookstore, book_id):
-    pass
+    for book_name, book in bookstore['books'].items():
+       if book['id'] is book_id:
+           return book
+       
 
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    bookslist = []
+    for book_name, book in bookstore['books'].items():
+      if book['author_id'] is author_id:
+          bookslist.append(book)
+    return bookslist
+        
+
+# PYTHONPATH=. py.test tests/test_bookstore.py::BookstoreTestCase::test_add_get_authors
