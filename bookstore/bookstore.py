5eaa4916009fcfad57b48b1cf82a8144b777cce6
Devan Govender <devakumarangovender@gmail.com>
2017-08-03 02:40:32 +0000

    Project Complete

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,62 @@
 def create_bookstore(name):
-    pass
+
+    bookstore = { 'name' : name, 
+                  'authors' : [], 
+                  'books' : []
+                }
+    return bookstore
 
 def add_author(bookstore, name, nationality):
-    pass
-
+    _id = id(name)
+    author = {'name' : name, 
+              'nationality' : nationality, 
+              'id' : _id}
+    bookstore['authors'].append(author)
+    return author
 
 def get_author_by_name(bookstore, name):
-    pass
-
+    authors = bookstore['authors']
+    for author in authors:
+        if author['name'] == name:
+            break
+    return author
 
 def get_author_by_id(bookstore, author_id):
-    pass
-
+    authors = bookstore['authors']
+    for author in authors:
+        if author_id == author['id']:
+            break
+    return author
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
-
+    _id = id(isbn)
+    book = {'title' : title, 
+            'isbn' : isbn, 
+            'author_id' : author_id,
+            'id': _id
+            }
+    bookstore['books'].append(book)
+    return book
 
 def get_book_by_title(bookstore, title):
-    pass
-
+    books = bookstore['books']
+    for book in books:
+        if book['title'] == title:
+            break
+    return book
 
 def get_book_by_id(bookstore, book_id):
-    pass
-
+    books = bookstore['books']
+    for book in books:
+        if book_id == book['id']:
+            break
+    return book
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    books = bookstore['books']
+    books_by_author = []
+    for book in books:
+        if book['author_id'] == author_id:
+            books_by_author.append(book)
+    return books_by_author
+    
