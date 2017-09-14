0e7ae50732c8c12f3d326aa37ebbef03318f59e0
Andrew Platt <andrew.platt91@gmail.com>
2017-09-14 22:36:14 +0000

    Final with Corrections

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,82 +1,65 @@
+from collections import defaultdict
+
 def create_bookstore(name):
     # Create the bookstore dictionary where names, authors, and books will be stored
-    bookstore = {}
-    
-    # Create a key 'name' where the bookstore name will be stored
-    bookstore['name'] = name
-    # return bookstore to user
+    bookstore = {'name': name, 'authors':[], 'books':[]}
     return bookstore
 
 def add_author(bookstore, name, nationality):
-    # Adding the author to the bookstore dictionary
-    bookstore[name] = {'name': name, 'nationality': nationality,'id':hash(name)}
-    
-    # Returning dictionary which contains the authors information
-    return bookstore[name]
-
-
+   
+   bookstore.get('authors').append({'name': name, 'nationality': nationality,'id':id(name)})
+   
+   return bookstore['authors'][-1]
+   
 def get_author_by_name(bookstore, name):
-    # We store authors by name in the dictionary we search for
-    # the key matching the name
-    for key in bookstore.keys():
-        if key == name:
-            
-            # Return the dictionary with all the authors information
-            return bookstore[key]
-
+    # We store authors dict in a list in the bookstore dictionary we search for
+    # 
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
 
 def get_author_by_id(bookstore, author_id):
     
-    # First we go through the bookstore dictionary
-    for bookstore_value in bookstore.values():
-        
-        # If the value is a dictionary we continue
-        if type(bookstore_value) is dict:
             
-            # We go through the dictionary filled with author information
-            for author_key in bookstore_value.keys():
+    # We go through the dictionary filled with author information
+    for author in bookstore['authors']:
                 
-                # When we find the matching ID we return the entire authors dictionary
-                if bookstore_value[author_key] == author_id:
-                    return bookstore_value
-
+        # When we find the matching ID we return the entire authors dictionary
+        if author['id'] == author_id:
+            return author
 
 def add_book(bookstore, title, isbn, author_id):
     
     #adds the title to the bookstore dictionary
-    bookstore[title] = {'title': title , 'isbn': isbn,'author_id': author_id, 'id': id(title)}
-    return bookstore[title]
+    bookstore.get('books').append({'title': title , 'isbn': isbn,'author_id': author_id, 'id': id(title)})
+    
+    return bookstore['books'][-1]
 
 def get_book_by_title(bookstore, title):
     
     # for each dict in bookstore 
-    for book_titles in bookstore.keys():
+    for book in bookstore['books']:
         
-        if book_titles == title:
+        if book['title'] == title:
 
-            return bookstore[book_titles]
+            return book
 
 def get_book_by_id(bookstore, book_id):
     
-    for book_titles in bookstore.values():
-       
-       if type(book_titles) is dict:
-           
-          for book_data in book_titles.keys():
-              
-              if book_titles[book_data] == book_id:
-                  return book_titles
-              
-            
+    # for each dict in bookstore 
+    for books in bookstore['books']:
+        
+        if books['id'] == book_id:
+
+            return books
 
 def get_books_by_author(bookstore, author_id):
+    #sets up the final list for the two names
     all_titles = []
-    for book_titles in bookstore.values():
-       
-       if type(book_titles) is dict and 'nationality' not in book_titles:
-          
-          for book_data in book_titles.keys():
-              
-            if book_titles[book_data] == author_id:
-                all_titles.append(book_titles)
+    
+    for book in bookstore['books']:
+        if book['author_id'] == author_id:
+                all_titles.append(book)
+                
     return all_titles
+
