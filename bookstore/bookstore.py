80d8471518e5a030fd5b1d4cee7b82892a176849
Adriana <aspatterson@gmail.com>
2017-10-31 23:49:55 +0000

    Bookstore solution

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,73 @@
+import pprint
+
 def create_bookstore(name):
-    pass
+    return {
+    'name' : name,
+    'authors' : [],
+    'books' : [],
+    'last_author_id': 0,
+    'last_book_id': 0,
+    }
+    
 
 def add_author(bookstore, name, nationality):
-    pass
-
-
+    dict1 = {}
+    dict1['nationality'] = nationality
+    dict1['name'] = name 
+    Iden = bookstore['last_author_id'] + 1
+    dict1['id'] = Iden
+    bookstore['authors'].append(dict1)
+    bookstore['last_author_id'] += 1
+    return dict1
+    
 def get_author_by_name(bookstore, name):
-    pass
-
+    list_authors = bookstore.get('authors')
+    for i in list_authors:
+        if i['name'] == name:
+            return i
+    return "Sorry author not found"
 
 def get_author_by_id(bookstore, author_id):
-    pass
-
-
+    list_ids = bookstore.get('authors')
+    for i in list_ids:
+        if i['id'] == author_id:
+            return i
+    return "Sorry author not found"
+            
 def add_book(bookstore, title, isbn, author_id):
-    pass
-
-
+    dict2 = {}
+    dict2['title'] = title
+    dict2['isbn'] = isbn 
+    author = get_author_by_id(bookstore, author_id)
+    authorsid = author['id']
+    authorsname = author['name']
+    dict2['author_id'] = authorsid
+    dict2['author'] = authorsname
+    Iden = bookstore['last_book_id'] + 1
+    dict2['id'] = Iden
+    bookstore['books'].append(dict2)
+    bookstore['last_book_id'] += 1
+    return dict2
+    
 def get_book_by_title(bookstore, title):
-    pass
-
-
+    list_books = bookstore.get('books')
+    for i in list_books:
+       if i['title'] == title:
+            return i
+    return "Sorry book not found"
+    
 def get_book_by_id(bookstore, book_id):
-    pass
-
-
+    list_ids = bookstore.get('books')
+    for i in list_ids:
+        if i['id'] == book_id:
+            return i
+    return "Sorry book not found"
+    
 def get_books_by_author(bookstore, author_id):
-    pass
+    list_ids = bookstore.get('books')
+    total_books = []
+    for i in list_ids:
+        if i['author_id'] == author_id:
+            total_books.append(i)
+    return total_books
+
