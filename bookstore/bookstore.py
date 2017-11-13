f035ae19cc191f5c113a02691d2bfbb0f70caa34
Adriana <adriana.verhagen@gmail.com>
2017-11-13 14:09:56 +0000

    updated

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -8,43 +8,43 @@ def create_bookstore(name):
     }
 
 def add_author(bookstore, name, nationality):
-    bookstore[last_author_id]+=1
+    bookstore['last_author_id']+=1
     author = {'name': name, 
               'nationality': nationality,
               'id': bookstore['last_author_id'] }
     bookstore['authors'].append(author)
-    return bookstore
+    return author
 
 def get_author_by_name(bookstore, name):
-    for name in bookstore['authors']:
-        if bookstore['name']==name:
+    for author in bookstore['authors']:
+        if author['name']==name:
             return author
 
 
 def get_author_by_id(bookstore, author_id):
-    for authord_id in bookstore['authors']:
-        if bookstore['id'] == authord_id:
+    for author in bookstore['authors']:
+        if author['id'] == author_id:
             return author
 
 
 def add_book(bookstore, title, isbn, author_id):
-    bookstore[last_book_id]+=1
-    book={'title':title,
+    bookstore['last_book_id']+=1
+    book = {'title':title,
           'isbn':isbn,
-          'authord_id':authord_id,
-          'book_id': bookstore[last_book_id]}
+          'author_id':author_id,
+          'book_id': bookstore['last_book_id']}
     bookstore['books'].append(book)
 
 
 def get_book_by_title(bookstore, title):
-    for title in bookstore['books']:
-        if bookstore[title]==title:
+    for book in bookstore['books']:
+        if book['title']==title:
             return book
 
 
 def get_book_by_id(bookstore, book_id):
-    for book_id in bookstore['books']:
-        if bookstore['book_id']==book_id:
+    for book in bookstore['books']:
+        if books['book_id']==book_id:
             return book
 
 
