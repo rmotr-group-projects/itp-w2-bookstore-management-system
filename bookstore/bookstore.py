436cc157b131c67758196bbcb8e785cc140254ae
RAYYYY <ray.sommer@gmail.com>
2017-12-07 23:31:54 +0000

    Bookstore Up and Running

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,50 @@
 def create_bookstore(name):
-    pass
+    name = {}
+    name['authors'] = []
+    name['books'] = []
+    return name
 
 
 def get_bookstore_name(bookstore):
-    pass
+    bookstore['name'] = "rmotr's bookstore"
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        'name': name,
+        'nationality': nationality
+    }
+    bookstore['authors'].append(author)
+    return author
 
-
-def get_author_by_name(bookstore, name):
-    pass
-
+def get_author_by_name(bookstore, name):#return author dict
+    find_author = name
+    for author in bookstore['authors']:
+        if author['name'] == find_author:
+            return author
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book = {
+        'title': title,
+        'isbn' : isbn,
+        'author': author
+    }
+    bookstore['books'].append(book)
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    find_book = title
+    for book in bookstore['books']:
+        if book['title'] ==  find_book:
+            return book
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    find_book_author = author
+    author_book_list = []
+    for book in bookstore['books']:
+        if book['author'] == find_book_author:
+            author_book_list.append(book)
+    return author_book_list
+    
