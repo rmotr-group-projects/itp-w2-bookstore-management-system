f3fbc1eb529aef457ee6a81991f469d2443e1ef2
Ernesto <ernesto.b.delacruz.jr@gmail.com>
2017-12-12 21:45:55 +0000

    Completed Assignment bookstore-management/All test passed.

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,40 @@
 def create_bookstore(name):
-    pass
-
+    bookstore = {'store': name, 'author': [], 'books': [] }
+    return bookstore
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore['store']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {'name':name, 'nationality':nationality}
+    bookstore['author'].append(author)
 
+    return author
 
 def get_author_by_name(bookstore, name):
-    pass
+    for author in bookstore['author']:
+        if name == author['name']:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
-
+    book = {'isbn':isbn, 'title':title, 'author':author}
+    bookstore['books'].append(book)
+    
+    return book
 
 def get_book_by_title(bookstore, title):
-    pass
+    for book in bookstore['books']:
+        if title == book['title']:
+            return book
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    authors_book_list = []
+    
+    for book_author in bookstore['books']:
+        if author == book_author['author']:
+            authors_book_list.append(book_author)
+
+    return authors_book_list
