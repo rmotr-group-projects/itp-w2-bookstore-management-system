03214c6d9136a8c1baa96c0e4d81a760bbef6294
Jordan Winfery <jwinf843@gmail.com>
2018-11-23 03:27:10 +0000

    Passing all tests and ready to upload to github

--- tests.py
+++ tests.py
@@ -7,6 +7,8 @@ def test_create_bookstore():
     store = create_bookstore("rmotr's bookstore")
     name = get_bookstore_name(store)
     assert name == "rmotr's bookstore"
+    
+
 
 
 def test_add_get_authors():
