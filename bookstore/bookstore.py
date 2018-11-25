de0c30100c2748c17284a2a01c268225a015ec75
julysses <julyssesd@gmail.com>
2018-11-25 20:07:55 +0000

    fixed typo in import

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -31,22 +31,22 @@ def add_book(bookstore, title, isbn, author):
     book = {
         'title': title,
         'isbn': isbn,
-        'author', author,
+        'author': author,
     }
     bookstore['book'].append(book)
     
 
 
 def get_book_by_title(bookstore, title):
-    for book in bookstore['books']:
+    for book in bookstore['book']:
         if book['title'] == title:
             return book
 
 
 def get_books_by_author(bookstore, author):
     books = []
-    for book in bookstore['books']:
+    for book in bookstore['book']:
         if book['author'] == author:
-            bgooks.append(book)
+            books.append(book)
             
     return books
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-pyfrom bookstore import (
+from bookstore import (
     create_bookstore, get_bookstore_name, get_book_by_title,
     get_author_by_name, add_author, add_book, get_books_by_author)
 
