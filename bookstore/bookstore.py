5e0cf6153a74f2b86b7e8c92a00447d4467f705f
sonia-b <sonia.a.buckley@gmail.com>
2019-01-25 03:54:15 +0000

    Passed all tests

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,53 @@
+# We are creating one store and chacking it a bunch of times
+# using store in the tests.py
 def create_bookstore(name):
-    pass
+    # You dont need to do return at the end.
+    return {
+        "name": name,
+        # we dont need a key value pair so using a list
+        "authors":[],
+        "books":[]
+    }
 
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        'name': name,
+        'nationality': nationality,
+    }
 
+    bookstore['authors'].append(author)
+    return author
 
 def get_author_by_name(bookstore, name):
-    pass
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
-
-
+    book = {
+        "title": title,
+        "isbn": isbn,
+        "author": author,
+    }
+    bookstore['books'].append(book)
+    # If you dont have this everything will return None.
+    return book
+    
 def get_book_by_title(bookstore, title):
-    pass
-
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
 
 def get_books_by_author(bookstore, author):
-    pass
+    books = []
+    for book in bookstore['books']:
+        if author == book['author']:
+            books.append(book)
+        
+    return books
