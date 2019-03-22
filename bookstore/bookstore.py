3b3b8db41c403e839b9977761d0665e39e4ba036
gmrkss <parlamasgeorge@gmail.com>
2019-03-22 02:21:13 +0000

    made changes to bookstore.py adding functionality for multiple things. Passing all tests

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,55 @@
 def create_bookstore(name):
-    pass
-
+    bookstore = {
+        'name' : name,
+        'authors' : [],
+        'books' : [],
+    }
+    
+    return bookstore
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore["name"]
 
 
 def add_author(bookstore, name, nationality):
-    pass
-
+    author={
+        'name':name,
+        'nationality':nationality
+    }
+    bookstore['authors'].append(author)
+    return author
 
 def get_author_by_name(bookstore, name):
-    pass
+    authors=bookstore['authors']
+    for author in authors:
+        if author['name'] == name:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book={
+        'title' : title,
+        'isbn' : isbn,
+        'author' : author,
+    }
+    bookstore['books'].append(book)
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
-
+    books = bookstore['books']
+    for book in books:
+        if book['title']==title:
+            return book
 
 def get_books_by_author(bookstore, author):
-    pass
+    results=[]
+    
+    books = bookstore['books'] 
+    
+    for book in books:
+        if book['author'] == author:
+            results.append(book)
+            
+    return results
+
