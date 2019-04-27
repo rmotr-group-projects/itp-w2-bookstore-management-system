5f9e6eb8dad8e103a105a8c047c08bb1aa663be2
naakaisa <naakaisa@gmail.com>
2019-04-27 15:12:45 +0000

    Add functionality bookstore.py

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,56 @@
 def create_bookstore(name):
-    pass
+    return{'name':name,
+           'authors': [],
+          }
 
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        'name': name,
+        'nationality': nationality
+    }
+    bookstore['authors'].append(author)
+    
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    author_list = bookstore['authors']
+    
+    for author in author_list:
+        if author['name'] == name:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
-
+    book = {
+        'title': title,
+        'isbn': isbn,
+        'author': author
+    }
+    
+    author_list = bookstore['authors']
+    for writer in author_list:
+        if writer['name'] == author:
+            writer['books'].append(book)
+            
+    bookstore['books'].append(book)
+    
+    return book
+        
 
 def get_book_by_title(bookstore, title):
-    pass
-
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
+        
 
 def get_books_by_author(bookstore, author):
-    pass
+    for writer in bookstore['authors']:
+        if writer['name'] == author:
+            return writer['books']
+        
