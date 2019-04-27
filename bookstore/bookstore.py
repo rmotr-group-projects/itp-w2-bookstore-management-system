ab86d39ad87d414a003854b5d1f841aa0b3c6752
naakaisa <naakaisa@gmail.com>
2019-04-27 15:21:47 +0000

    Add new functionality bookstore.py

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,7 +1,33 @@
 def create_bookstore(name):
-    return{'name':name,
-           'authors': [],
-          }
+    return {
+        'name': name,
+        'authors': [],
+        'books': [],
+    }
+
+'''
+bookstore = {
+    'name': "rmotr's bookstore",
+    'authors': [
+        {
+        'name': 'Edgar Allen Poe',
+        'nationality': 'US',
+        'books': [...]
+        },
+        {
+        'name': 'Jorge Luis Borges',
+        'nationality': 'AR',
+        'books': [...]
+        },
+        {
+        'name': "James Joyce",
+        'nationality', 'UK',
+        'books': [...]
+        },
+    ]
+    'books': [...]
+}
+'''
 
 
 def get_bookstore_name(bookstore):
@@ -11,11 +37,14 @@ def get_bookstore_name(bookstore):
 def add_author(bookstore, name, nationality):
     author = {
         'name': name,
-        'nationality': nationality
+        'nationality': nationality,
+        'books': []
     }
+    
     bookstore['authors'].append(author)
     
     return author
+    
 
 
 def get_author_by_name(bookstore, name):
@@ -30,7 +59,7 @@ def add_book(bookstore, title, isbn, author):
     book = {
         'title': title,
         'isbn': isbn,
-        'author': author
+        'author': author,
     }
     
     author_list = bookstore['authors']
@@ -39,9 +68,9 @@ def add_book(bookstore, title, isbn, author):
             writer['books'].append(book)
             
     bookstore['books'].append(book)
-    
+            
     return book
-        
+
 
 def get_book_by_title(bookstore, title):
     for book in bookstore['books']:
@@ -49,8 +78,9 @@ def get_book_by_title(bookstore, title):
             return book
         
 
+
 def get_books_by_author(bookstore, author):
+    
     for writer in bookstore['authors']:
         if writer['name'] == author:
             return writer['books']
-        
