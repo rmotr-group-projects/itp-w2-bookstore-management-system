f8ffecd34e14bfd40e56aa54c775dd14bc1c9db9
nicktlc <nicktlc@hotmail.com>
2019-06-27 18:20:24 +0000

    Worked with b34 to add functionality to bookstore.py file

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,55 @@
 def create_bookstore(name):
-    pass
-
+    return {
+        'shop name': name,
+        'authors' :[],
+        'books': []
+    }
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore['shop name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        'name' : name,
+        'nationality' : nationality ,
+        'books':[],
+    }
+    
+    bookstore['authors'].append(author)
+    
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    author_list = bookstore['authors']
+    for author in author_list:
+        if author['name'] == name:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book = {
+        'title': title,
+        'isbn' : isbn,
+        'author': author,
+    }
+    bookstore['books'].append(book)
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    book_list = bookstore['books']
+    for book in book_list:
+        if title.lower() in book['title'].lower():
+            return book
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    books = []
+    book_list = bookstore['books']
+    for book in book_list:
+        if author.lower() in book['author'].lower():
+            books.append(book)
+            
+    return books
